81d4849da21cdb175134ef66002b0362cd8b1705
Thomas Watson <w@tson.dk>
2018-09-09 01:43:15 +0200

    Revert "Remove RUN_ID from blessed list"
    This reverts commit 559fffdcad648875fdd28dfa82e75011276ce8e5.

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ exports.isCI = !!(
   env.CI || // Travis CI, CircleCI, Cirrus CI, Gitlab CI, Appveyor, CodeShip, dsari
   env.CONTINUOUS_INTEGRATION || // Travis CI, Cirrus CI
   env.BUILD_NUMBER || // Jenkins, TeamCity
+  env.RUN_ID || // TaskCluster, dsari
   exports.name ||
   false
 )
