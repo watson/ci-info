f5816acbe33bb2953907b847c93c7070acf41701
Leo Toneff <43667677+bragle@users.noreply.github.com>
2022-09-19 12:18:43 +0200

    Fixed Appflow comment typo

--- index.js
+++ index.js
@@ -57,9 +57,9 @@ exports.isCI = !!(
   env.CI || // Travis CI, CircleCI, Cirrus CI, Gitlab CI, Appveyor, CodeShip, dsari
   env.CONTINUOUS_INTEGRATION || // Travis CI, Cirrus CI
   env.BUILD_NUMBER || // Jenkins, TeamCity
-  env.CI_APP_ID || // Applfow
-  env.CI_BUILD_ID || // Applfow
-  env.CI_BUILD_NUMBER || // Applfow
+  env.CI_APP_ID || // Appflow
+  env.CI_BUILD_ID || // Appflow
+  env.CI_BUILD_NUMBER || // Appflow
   env.RUN_ID || // TaskCluster, dsari
   exports.name ||
   false
